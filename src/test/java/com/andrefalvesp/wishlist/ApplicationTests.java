848fe30ed6e64648fe6c46c46dbedd11dce36b86
andrefalvesp <andre_falvesp@hotmail.com>
2021-04-05 00:46:49 -0300

    [Application - tests]

---
+++ src/test/java/com/andrefalvesp/wishlist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrefalvesp.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
